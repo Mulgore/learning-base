5d5cc0ee08b5647a109214d52625d83cf57a587a
reawei <reawei@vip.qq.com>
2018-09-10 14:22:01 +0800

    update

---
+++ src/main/java/com/reawei/base/BaseDay02.java
@@ -0,0 +1,41 @@
+package com.reawei.base;
+
+/**
+ * Create in 13:37 2018/9/10
+ * JAVA基础学习第二天
+ * <p>
+ * 基本数据类型
+ *
+ * @author: qigong
+ */
+public class BaseDay02 {
+
+    /**
+     * 下节学习JAVA的运算符
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        //Java 分为8个基本类型
+        //整型 byte, short, int, long
+        //浮点型  float, double
+        //布尔类型 boolean
+        //字符型 char
+        byte a = 0;
+        short b = 0;
+        int c = 0;
+        long d = 0;
+        float e = 0.0f;
+        double f = 0.0d;
+        boolean g = true;
+        char h = 'a';
+        System.out.println("字节类型: " + a);
+        System.out.println("短整型: " + b);
+        System.out.println("整型: " + c);
+        System.out.println("长整型: " + d);
+        System.out.println("单精度浮点型: " + e);
+        System.out.println("双精度浮点型: " + f);
+        System.out.println("布尔类型: " + g);
+        System.out.println("字符型: " + h);
+    }
+}
