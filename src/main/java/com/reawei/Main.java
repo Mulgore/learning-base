a606e4ee723f4434c4e2c4ad5e2226c39ab52412
reawei <reawei@vip.qq.com>
2018-09-10 13:42:25 +0800

    JAVA 基础学习第一天

---
+++ src/main/java/com/reawei/Main.java
@@ -0,0 +1,17 @@
+package com.reawei;
+
+/**
+ * Create in 13:36 2018/9/10
+ *  JAVA 学习
+ * @author: qigong
+ */
+public class Main {
+
+    /**
+     * 主函数(方法)
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/com/reawei/base/BaseDay01.java
@@ -0,0 +1,19 @@
+package com.reawei.base;
+
+/**
+ * Create in 13:37 2018/9/10
+ * JAVA基础学习第一天
+ *
+ * @author: qigong
+ */
+public class BaseDay01 {
+
+    /**
+     * 下节学习JAVA的基本类型
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to JAVA ");
+    }
+}
