90c1c224a6877f77cb3063a82801af219c5c3755
reawei <reawei@vip.qq.com>
2018-09-11 13:52:35 +0800

    运算符

---
+++ src/main/java/com/reawei/base/BaseDay03.java
@@ -0,0 +1,31 @@
+package com.reawei.base;
+
+/**
+ * Create in 13:43 2018/9/11
+ * JAVA基础学习第三天
+ * <p>
+ * 关系运算符
+ *
+ * @author: qigong
+ */
+public class BaseDat03 {
+
+    /**
+     * 下节学习JAVA的关系运算符
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        System.out.println("加法: " + (a + b));
+        System.out.println("减法: " + (a - b));
+        System.out.println("乘法: " + (a * b));
+        System.out.println("除法: " + (a / b));
+        System.out.println("取余: " + (a % b));
+        System.out.println("自增: " + (a++));
+        System.out.println("自增: " + (++a));
+        System.out.println("自减: " + (a--));
+        System.out.println("自减: " + (--a));
+    }
+}
