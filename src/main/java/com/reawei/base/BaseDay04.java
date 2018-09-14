606ab687b4b6c45a8d4d1f5396ac0ac990eed48b
reawei <reawei@vip.qq.com>
2018-09-14 09:33:34 +0800

    update

---
+++ src/main/java/com/reawei/base/BaseDay04.java
@@ -0,0 +1,22 @@
+package com.reawei.base;
+
+/**
+ * Create in 09:29 2018/9/14
+ * JAVA基础学习第四天
+ * 关系运算符
+ *
+ * @author: qigong
+ */
+public class BaseDay04 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("a == b = " + (a == b));
+        System.out.println("a != b = " + (a != b));
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));
+        System.out.println("b >= a = " + (b >= a));
+        System.out.println("b <= a = " + (b <= a));
+    }
+}
