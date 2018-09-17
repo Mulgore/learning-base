b41d5158df2babe2259e634f787a93e948297d3b
reawei <reawei@vip.qq.com>
2018-09-17 10:38:04 +0800

    update

---
+++ src/main/java/com/reawei/base/BaseDay05.java
@@ -0,0 +1,19 @@
+package com.reawei.base;
+
+/**
+ * Create in 09:29 2018/9/14
+ * JAVA基础学习第五天
+ * 逻辑运算符
+ *
+ * @author: qigong
+ */
+public class BaseDay05 {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a&&b));
+        System.out.println("a || b = " + (a||b) );
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+}
